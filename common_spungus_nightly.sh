3e959f50d3efee0d06988ac9eba1cabdef5c9b54
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-08 16:27:07 -0700

    Update common_spungus_nightly.sh

--- common_spungus_nightly.sh
+++ common_spungus_nightly.sh
@@ -2,17 +2,17 @@
 common_spungus(){ sh|sh;};
 advanced_spungus(){ awk '
 function f(x,y){if($1==x){print y";";e=0}}
-$0=="advanced_spungus"{s=!s;next};!s;s{
-e=1;a=$1;b=$2;c=$3;
-f("assign",b"="c$4$5)
-f("declare",b" "c)
-f("malloc",b"="a"("c")")
-f("free",a"("b")")
-f("control",b"("c"){")
-f("case",a" "b":")
-f("break",a)
-f("default",a":")
-f("}",a)
-if(e||b c$5!~/^[a-zA-Z0-9_()*]*$/||
-b c$5~/[a-zA-Z0-9_\)]\(/||
-$4!~/^[<>\/*%&|!=|+-]*$/)exit 1}';};
+$0=="advanced_spungus"{s=!s;next};!s;s{e=1;
+f("assign",$2"="$3$4$5)
+f("declare",$2" "$3)
+f("malloc",$2"="$1"("$3")")
+f("free",$1"("$2")")
+f("control",$2"("$3"){")
+f("case",$1" "$2":")
+f("break",$1)
+f("default",$1":")
+f("}",$1)
+if(e||$2$3$5!~/^[a-zA-Z0-9_()*]*$/||
+$2$3$5~/[a-zA-Z0-9_\)]\(/||
+$4!~/^[<>\/*%&|!=|+-]*$/){
+print ";=;";exit 1}}';};
