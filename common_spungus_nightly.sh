e462975b8cbc80e27d66be26547fe2b62115a108
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-08 17:06:12 -0700

    Update common_spungus_nightly.sh

--- common_spungus_nightly.sh
+++ common_spungus_nightly.sh
@@ -3,11 +3,12 @@ common_spungus(){ sh|sh;};
 advanced_spungus(){ awk '
 function f(x,y){if($1==x){print y";";e=0}}
 $0=="advanced_spungus"{s=!s;next};!s;s{e=1;
-f("assign",$2"="$3$4$5)
 f("declare",$2" "$3)
+f("assign",$2"="$3$4$5)
 f("malloc",$2"="$1"("$3")")
 f("free",$1"("$2")")
-f("control",$2"("$3"){")
+f("switch",$1"("$2"){")
+f("while",$1"("$2"){")
 f("case",$1" "$2":")
 f("break",$1)
 f("default",$1":")
