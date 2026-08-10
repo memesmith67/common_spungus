59781119b815410d9edfb5a6af8381ba9b78fdf7
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-09 22:42:13 -0700

    Update common_spungus.sh

--- common_spungus.sh
+++ common_spungus.sh
@@ -1,19 +1,2 @@
 #copyright john morris beck 2026 gpl2
 common_spungus(){ sh|sh;};
-advanced_spungus(){ awk '
-function f(x,y){if($1==x){print y;e=0}}
-$0=="advanced_spungus"{s=!s;next}!s;s{e=1;
-f("declare",$2" "$3";")
-f("assign",$2"="$3$4$5";")
-f("malloc",$2"="$1"("$3");")
-f("free",$1"("$2");")
-f("switch",$1"("$2"){")
-f("while",$1"("$2"){")
-f("case",$1" "$2":")
-f("break",$1";")
-f("default",$1":")
-f("}",$1)
-if(e||$2$3$5!~/^[a-zA-Z0-9_()*]*$/||
-$2$3$5~/[a-zA-Z0-9_\)]\(/||
-$4!~/^[<>\/*%&|!=|+-]*$/){
-print ";=;";exit 1}}';};
